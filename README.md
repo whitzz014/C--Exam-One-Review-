1dff63aafcc90161f8b8cf901135290ea11f188e
Brock <w0781391@myscc.ca>
2024-02-22 23:27:03 -0500

    Customer class created

--- README.md
+++ README.md
@@ -76,6 +76,29 @@
 ```
 ### Customer Class
 
+```
+    class Customer : Person
+    {
+        //Properties
+        public int CustomerNumber;
+        public bool OnMailingList;
+
+        //initalizing name, address, phoneNumber from parent class 
+        public Customer(int customerNumber, bool onMailingList, string name, string address, string phoneNumber ) 
+            : base(name, address, phoneNumber)
+        {
+            CustomerNumber = customerNumber;
+            OnMailingList = onMailingList;
+        }
+
+        //samething as parent 
+        public override string ToString()
+        {
+            return $"Customer Number: {CustomerNumber}, On Mailing List? {IsOnMailingList}";
+        }
+    }
+```
+
 ## Assignment 5 - War Card Game 
  
     - class deck, card, player, game, suit (enum), Round 
---
+++ Test1Review/Test1Review/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1Review
+{
+    class Customer : Person
+    {
+        //Properties
+        public int CustomerNumber;
+        public bool OnMailingList;
+
+        //initalizing name, address, phoneNumber from parent class 
+        public Customer(int customerNumber, bool onMailingList, string name, string address, string phoneNumber ) 
+            : base(name, address, phoneNumber)
+        {
+            CustomerNumber = customerNumber;
+            OnMailingList = onMailingList;
+        }
+
+        //samething as parent 
+        public override string ToString()
+        {
+            return $"Customer Number: {CustomerNumber}, On Mailing List? {IsOnMailingList}";
+        }
+    }
+}
