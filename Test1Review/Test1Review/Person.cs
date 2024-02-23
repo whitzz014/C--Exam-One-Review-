168e20e045e7727b44c25b6bd409f6b24bfae579
Brock <w0781391@myscc.ca>
2024-02-22 23:12:37 -0500

    adding code to README and solution

---
+++ Test1Review/Test1Review/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1Review
+{
+    class Person
+    {
+        //Properties ++ Getters & Setters
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+
+        //Constructor
+        public Person(string name, string address, string phoneNumber)
+        {
+            Name = name;
+            Address = address;
+            PhoneNumber = phoneNumber;
+        }
+
+        // Override the default ToString method to provide a custom string representation of the object.
+        public override string ToString()
+        {
+            //Format the output to be this string 
+            return $"Name: {Name}, Address: {Address}, Phone Number: {PhoneNumber}";
+        }
+    }
+}
